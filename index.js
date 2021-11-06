f5b93a0127f91420a7681e6133f7ccc31aac03fa
jlaguilarc <jos05_12@hotmail.com>
2021-11-06 08:20:36 -0500

    plantilla index pug funcionando

--- index.js
+++ index.js
@@ -3,24 +3,40 @@ const mensaje = require("./Mensaje");
 
 const app = express();
 
-//Definiendo una ruta
-app.get("/ping",(request, response) =>{
-    response.send("pong");
-});
+// //Definiendo una ruta
+// app.get("/ping",(request, response) =>{
+//     response.send("pong");
+// });
 
-app.get("/", (request,response)=>{
-    response.send("Página principal");
-});
+// app.get("/", (request,response)=>{
+//     response.send("Página principal");
+// });
 
-app.get("/contacto", function(req,res){
-    //res.send("Ruta de contactenos");
-    res.send("Hola "+req.query.nombre);
-});
+// app.get("/contacto", function(req,res){
+//     //res.send("Ruta de contactenos");
+//     res.send("Hola "+req.query.nombre);
+// });
 
-app.post("/registro", function(req,res){
+// app.post("/registro", function(req,res){
 
-});
+// });
 
-express().use("/",mensaje({saludo: "Hola Mundo web"})).listen(8080);
+// express().use("/",mensaje({saludo: "Hola Mundo web"})).listen(8080);
 
 //app.listen(8080, "localhost");    
+const PORT = 3030;
+
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+app.get("/", function(req, res){
+    res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+
+app.listen(PORT, function(err){
+    if(!err){
+        console.log("Servidor corriendo puerto ",PORT);
+    }else{
+        console.log(err);
+    }
+})
